4f9ca5bdd84aeeedb9ae6ea3ac55534b5b6b5ee6
gregorymayo <gregoryjuevesii23@gmail.com>
2019-02-06 01:43:43 -0800

    First commit

---
+++ flixster/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  flixster
+//
+//  Created by Gregory Jueves Mayo on 2/6/19.
+//  Copyright © 2019 Gregory Jueves Mayo. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
